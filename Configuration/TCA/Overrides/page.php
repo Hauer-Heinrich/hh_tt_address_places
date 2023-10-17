9f05e0eb2206f277855e204243f9040f3600e212
Christian Hackl <hackl.chris@googlemail.com>
2023-10-17 16:58:56 +0200

    [TASK] added page tsconfig for people and places

--- Configuration/TCA/Overrides/page.php
+++ Configuration/TCA/Overrides/page.php
@@ -12,4 +12,16 @@ call_user_func(function() {
         'Configuration/TsConfig/AllPage.typoscript',
         'hh_tt_address_places - Places / Companies'
     );
+
+    ExtensionManagementUtility::registerPageTSConfigFile(
+        $extensionKey,
+        'Configuration/TsConfig/OnlyPeople.typoscript',
+        'hh_tt_address_places - Allow only people (for storage location)'
+    );
+
+    ExtensionManagementUtility::registerPageTSConfigFile(
+        $extensionKey,
+        'Configuration/TsConfig/OnlyPlaces.typoscript',
+        'hh_tt_address_places - Allow only Places / Companies (for storage location)'
+    );
 });
