472cd3c440e3b4657630598438bfd37b95e7bd5d
Christian Hackl <hackl.chris@googlemail.com>
2025-02-04 12:05:50 +0100

    [TASK] maintaince

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -3,12 +3,10 @@ defined('TYPO3') || die();
 
 use \TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
-call_user_func(function() {
-    $extensionKey = 'hh_tt_address_places';
-
+call_user_func(function(string $extensionKey) {
     ExtensionManagementUtility::addStaticFile(
         $extensionKey,
         'Configuration/TypoScript',
         'Address places'
     );
-});
+}, 'hh_tt_address_places');
