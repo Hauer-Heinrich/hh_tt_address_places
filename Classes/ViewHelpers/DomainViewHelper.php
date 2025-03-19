4d4af6c949e46eaef86ddbb272133875bc11f519
Christian Hackl <hackl.chris@googlemail.com>
2025-03-19 10:25:37 +0100

    [TASK] added DomainViewHelper
    e. g. is used, for example, for more beautiful URL output in the FE

---
+++ Classes/ViewHelpers/DomainViewHelper.php
@@ -0,0 +1,64 @@
+<?php
+declare(strict_types=1);
+
+namespace HauerHeinrich\HhTtAddressPlaces\ViewHelpers;
+
+// use \TYPO3\CMS\Extbase\Utility\DebuggerUtility;
+use \TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+use \TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+
+
+/**
+ * DomainViewHelper
+ *
+ * usage e.g. for tt_address field "www":
+ * {address.www} = "https://www.domain.tld/some-site" or "t3://page?uid=79 _blank my-class link-title"
+ * returns "www.domain.tld"
+ *
+ * <html xmlns:f="http://typo3.org/ns/TYPO3/CMS/Fluid/ViewHelpers"
+ *       xmlns:place="http://typo3.org/ns/HauerHeinrich/HhTtAddressPlaces/ViewHelpers"
+ *       data-namespace-typo3-fluid="true">
+ *
+ * <place:domain url="{f:uri.typolink(parameter: '{address.www}', absolute: 1)}" domainOnly="1" withoutScheme="1" />
+ */
+class DomainViewHelper extends AbstractViewHelper {
+
+    public function initializeArguments() {
+        $this->registerArgument('url', 'string', '', true);
+        $this->registerArgument('domainOnly', 'bool', 'Return only the domain host, eg. www.domain.tld ', false, false);
+        $this->registerArgument('withoutScheme', 'bool', 'Return url without "scheme"', false, false);
+    }
+
+    /**
+     * @param array $arguments
+     * @param \Closure $renderChildrenClosure
+     * @param RenderingContextInterface $renderingContext
+     *
+     * @return void
+     */
+    public static function renderStatic(array $arguments, \Closure $renderChildrenClosure, RenderingContextInterface $renderingContext) {
+        $givenUrl = $arguments['url'];
+        $domainOnly = $arguments['domainOnly'];
+        $withoutScheme = $arguments['withoutScheme'];
+
+        if(filter_var($givenUrl, FILTER_VALIDATE_URL)) {
+            $urlParts = parse_url($givenUrl);
+
+            $host = $urlParts['host'];
+
+            if($domainOnly === true && $withoutScheme === true) {
+                return $host;
+            }
+
+            if($domainOnly === true) {
+                return $urlParts['scheme'] . '://' . $host;
+            }
+
+            if($withoutScheme === true) {
+                return $host . $urlParts['path'];
+            }
+        }
+
+        return $givenUrl;
+    }
+}
