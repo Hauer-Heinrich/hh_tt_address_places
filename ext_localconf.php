1887c82333dbd104b8b19e9c74f5548e2a9a6523
Christian Hackl <hackl.chris@googlemail.com>
2025-05-08 15:13:46 +0200

    [FIX] register upgradeWizard for field place migration to parentid / parenttable

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,4 +5,8 @@ defined('TYPO3') || die();
     $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\FriendsOfTYPO3\TtAddress\Domain\Model\Address::class] = [
         'className' => \HauerHeinrich\HhTtAddressPlaces\Domain\Model\Place::class,
     ];
+
+    // Register UpdateWizards
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['hhttaddressplaces_parentIdMigrationWizard']
+        = \HauerHeinrich\HhTtAddressPlaces\Upgrades\ParentIdMigrationWizard::class;
 })();
